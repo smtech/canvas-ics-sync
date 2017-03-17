1fc153a31e6fa4c2a2fedf54209864b9aa3fa9fa
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2017-03-17 21:56:42 +0100

    Scrutinizer Auto-Fixes (#23)
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- export.php
+++ export.php
@@ -3,27 +3,27 @@
 require_once('common.inc.php');
 
 if (isset($_REQUEST['course_url'])) {
-	$courseId = preg_replace('|.*/courses/(\d+)/?.*|', '$1', parse_url($_REQUEST['course_url'], PHP_URL_PATH));
-	$course = $api->get("/courses/$courseId");
-	if ($course) {
-		$webcalFeed = str_replace('https://', 'webcal://', $course['calendar']['ics']);
-		$smarty->assign('content', '
+    $courseId = preg_replace('|.*/courses/(\d+)/?.*|', '$1', parse_url($_REQUEST['course_url'], PHP_URL_PATH));
+    $course = $api->get("/courses/$courseId");
+    if ($course) {
+        $webcalFeed = str_replace('https://', 'webcal://', $course['calendar']['ics']);
+        $smarty->assign('content', '
 		<h3>Course Calendar ICS Feed</h3>
 		<p>You can subscribe to the calendar for <a href="https://' .
-		parse_url(CANVAS_API_URL, PHP_URL_HOST) . '/courses/' . $courseId .
-		'">' . $course['name'] . '</a> at <a href="' .
-		$webcalFeed . '">' . $webcalFeed .
-		'</a> in any calendar application that supports external ICS feeds.</p>'
-		);
-	} else {
-		$messages[] = array(
-			'class' => 'error',
-			'title' => 'Canvas API Error',
-			'content' => 'The course you requested could not be accessed.<pre>' . print_r($json, false) . '</pre>'
-		);
-	}
+        parse_url(CANVAS_API_URL, PHP_URL_HOST) . '/courses/' . $courseId .
+        '">' . $course['name'] . '</a> at <a href="' .
+        $webcalFeed . '">' . $webcalFeed .
+        '</a> in any calendar application that supports external ICS feeds.</p>'
+        );
+    } else {
+        $messages[] = array(
+            'class' => 'error',
+            'title' => 'Canvas API Error',
+            'content' => 'The course you requested could not be accessed.<pre>' . print_r($json, false) . '</pre>'
+        );
+    }
 } else {
-	$smarty->assign('content', '
+    $smarty->assign('content', '
 	<form method="post" action="' . $_SERVER['PHP_SELF'] . '">
 		<label for="course_url">Course URL <span class="comment">The URL to the course whose calendar you would like to export as an ICS feed</span></label>
 		<input id="course_url" name="course_url" type="text" />
